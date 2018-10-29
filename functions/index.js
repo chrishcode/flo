14dcba261db1aae8c5710a7b9cf45158564b5408
chrisWohlfarth <chris.wohlfarth@gmail.com>
2018-10-29 21:18:34 +0100

    add flo art and checkbox input

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,30 @@
 const functions = require('firebase-functions');
+const chalk = require('chalk');
+const clear = require('clear');
+const figlet = require('figlet');
+const files = require('../lib/files');
+const inquirer = require('../lib/inquirer');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+ response.send("Hello from Firebase!");
+});
+
+clear();
+console.log(chalk.hex('#F50064')
+  (figlet.textSync('flo', {
+    font: 'Blocks',
+    horizontalLayout: 'default',
+    verticalLayout: 'default'
+  })
+  )
+);
+
+const run = async () => {
+  const credentials = await inquirer.askGithubCredentials();
+  console.log(credentials);
+}
+
+run();
