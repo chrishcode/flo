427f03a744b7b3bb71d83cdd3314d1fb0d2d03ab
chrisWohlfarth <chris.wohlfarth@gmail.com>
2018-10-31 16:45:47 +0100

    make the cli generate components

--- functions/index.js
+++ functions/index.js
@@ -4,34 +4,26 @@ const clear = require('clear');
 const figlet = require('figlet');
 const files = require('../lib/files');
 const inquirer = require('../lib/inquirer');
-const touch = require("touch");
-var fs = require('fs'); //https://node.readthedocs.io/en/latest/api/fs/
 
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+//  response.send("Hello from Firebase!");
 });
 
 clear();
+
 console.log(chalk.hex('#F50064')
   (figlet.textSync('flo', {
     font: 'Blocks',
     horizontalLayout: 'default',
     verticalLayout: 'default'
-  })
-  )
+  }))
 );
 
 const run = async () => {
   const componentsToScaffold = await inquirer.askWhatComponentsToScaffold();
-
-  if(componentsToScaffold) {
-    for (var i = 0; i < componentsToScaffold.length; i++) {
-      console.log(componentsToScaffold[i]);
-    }
-  }
 }
 
 run();
