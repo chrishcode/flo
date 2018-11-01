ef5933d8c1b3c51e0b6514bb0229a1bdb5e81413
chrisWohlfarth <chris.wohlfarth@gmail.com>
2018-11-01 18:37:59 +0100

    copy components from remote git repo

--- functions/index.js
+++ functions/index.js
@@ -5,6 +5,7 @@ const clear = require('clear');
 const figlet = require('figlet');
 const inquirer = require('inquirer');
 const fs = require('fs');
+const request = require('request');
 
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -24,19 +25,27 @@ console.log(chalk.hex('#F50064')
 );
 
 function askWhatComponentsToScaffold() {
-  const questions = [{
-    type: 'checkbox',
-    name: 'componentsToScaffold',
-    message: 'What components do you want to scaffold?',
-    choices: ['Button', 'ShoppingCart', 'Card']
-  }];
+  const questions = [
+    // {
+    //   type: 'list',
+    //   name: 'starterAppOrComponent',
+    //   message: 'Do you want to scaffold a starter app or a component?',
+    //   choices: ['Starter app', 'Component']
+    // },
+    {
+      type: 'checkbox',
+      name: 'componentsToScaffold',
+      message: 'What components do you want to scaffold?',
+      choices: ['Button']
+    }
+  ];
 
   inquirer.prompt(questions).then(answers => {
     var componentsToScaffold = answers.componentsToScaffold;
 
     if(componentsToScaffold) {
       for (var i = 0; i < componentsToScaffold.length; i++) {
-        fs.createReadStream('components/Button.js').pipe(fs.createWriteStream(componentsToScaffold[i] + '.js'));
+        request('https://raw.githubusercontent.com/chrishcode/flo/master/functions/components/react' + componentsToScaffold[i] + '.js').pipe(fs.createWriteStream(componentsToScaffold[i] + '.js'));
       }
     }
   });
